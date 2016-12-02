1a212d7513d9d7c7d18df2eec052e8be65761c78
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2016-12-02 14:34:41 -0300

    Terminado, funciona con los casos de prueba

--- ejercicio2.c
+++ ejercicio2.c
@@ -40,7 +40,7 @@ float arbolOptimo(int numero, float * arregloProba){
 			
 			valorMinimo = 1000000;
 			for(k = i; k <= j ; k++){
-				//printf("i - 1: %d\n  k - 1 : %d\n  k: %d\n  j:  %d\n",i-1,k-1,k,j);
+				
 				if(matriz[i-1][k-1] + matriz[k][j] < valorMinimo){
 					
 					valorMinimo = matriz[i-1][k-1] + matriz[k][j];
@@ -48,36 +48,42 @@ float arbolOptimo(int numero, float * arregloProba){
 				}
 
 			}
-			
-
-			/** Acá está el error **/
-
 			suma = 0;
 			
 			for(s = i; s <= j; s++){
 				suma += arregloProba[s-1];
 			}
 			
-			/** Acá está el error **/
+			
 
 			matriz[i-1][j] = valorMinimo + suma;
-		
-		
 		}
-	
-
 	}
 
 	return matriz[0][numeroUtil];
 }
 
 int main(){
-	float array[4];
-	array[0] = 0.23;
-	array[1] = 0.4;
-	array[2] = 0.17;
-	array[3] = 0.2;
-	float resultado = arbolOptimo(5, array);
-	printf("%.4f\n",resultado);
+	int i;
+	int numeroNodos;
+	int counter = 0;
+	float array[101];
+	float resultado;
+	FILE * entrada = stdin;
+	while(!(feof(entrada))){
+		if(counter % 2 == 0){
+			fscanf(entrada,"%d",&numeroNodos);
+			counter++;
+		}
+		else if(counter % 2 == 1){
+			for(i = 0; i < numeroNodos; i++){
+				fscanf(entrada,"%f ",&array[i]);
+			}
+			resultado = arbolOptimo(numeroNodos + 1, array);
+			printf("%.4f\n",resultado);
+			counter++;
+		}
+	}
+	
 	return 0;
 }
